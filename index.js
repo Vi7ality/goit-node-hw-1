3629c436fef645092d6e9930424d7a5293b82888
Vitalii <svitalii138@gmail.com>
2023-02-16 13:32:01 +0200

    Init

---
+++ index.js
@@ -0,0 +1,58 @@
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  removeContact,
+} = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const list = await listContacts();
+      console.log(list);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not exist`);
+      }
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "update":
+      const updatedContact = await updateContact(id, name, email, phone);
+      if (!updateContact) {
+        throw new Error(`Contact with id=${id} not exist`);
+      }
+      console.log(updatedContact);
+    case "remove":
+      const removedContact = await removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
